da9fc539f0330da32fc255e1ab9ccc20dcd9fea6
KIDx <972169909@qq.com>
2014-05-19 11:50:38 +0800

    change port to 5000

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var app = express()
 ,	socketID = {};
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 5000);
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
 //app.use(express.favicon());
